eb12112e27c75735aba4cd1ef43ddb6710178e11
Niki691 <nklaerum@t-online.de>
2021-11-05 13:00:40 +0100

    Merge canvas and frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,88 +1,19 @@
-import React from 'react';
-import CytoscapeComponent from 'react-cytoscapejs'
-import dagre from 'cytoscape-dagre'
-import cytoscape from 'cytoscape'
-import { epc } from './formats/epc.js';
-import { dfg } from './formats/dfg.js';
-import { diagramXML } from './diagram.js';
-import axios from 'axios'
-import './App.css';
-import ReactBpmn from 'react-bpmn';
+import "./App.css";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import { ThemeProvider } from "@ui5/webcomponents-react";
 
-
-cytoscape.use( dagre );
-
-export default function App() {
-  // Set the layout of the graph
-  const layout = {name: 'dagre'};
-
-  // True if the user wanna see the BPMN graph
-  var isBPMN = false;
-
-  // True if the user wanna see the DFG graph
-  var isDFG = true;
-
-  // Query Graph from the backend
-  const [dfgGraph, setDFGGraph] = React.useState({});
-  const [epcGraph, setEPCGraph] = React.useState({});
- //const [bpmnGraph, setBPMNGraph] = React.useState({});
-
-  // Status messages for the BPMN graph in the browser console
-    function onShown() {
-      console.log('diagram shown');
-    }
-  
-    function onLoading() {
-      console.log('diagram loading');
-    }
-  
-    function onError(err) {
-      console.log('failed to show diagram');
-    }
-  
-  // Fetch graph from node backend
-  async function fetchGraph() {
-     await axios.post("/graph/variants", {variants: [], sequence: ""}, {params: {id: 12}},
-     {headers: {"Access-Control-Allow-Origin": "*"}}
-     )
-    .then((response) => {
-      setDFGGraph(response.data.dfg.graph)
-      setEPCGraph(response.data.epc.graph)
-      //setBPMNGraph(response.data.bpmn.graph)
-    })
-    .catch(err => {
-      console.log(err)
-    })
-    
-  }
-
-  // Use the fetchGraph function
-  React.useEffect(() => {
-    fetchGraph();
-  }, [])
-  
-  // Choose the right styling and the right graph
-  let style = isDFG ? dfg : epc;
-  let graph = isDFG ? dfgGraph : epcGraph;
-  
-  // Uses bpmn-js if its a bpmn graph otherwise it uses cytoscape
-  return isBPMN ? (<ReactBpmn class="diagram-container"
-                  diagramXML={ diagramXML }
-                  onShown={ onShown }
-                  onLoading={ onLoading }
-                  onError={ onError }
-                  />)
-                  : 
-                  <CytoscapeComponent 
-                    wheelSensitivity={0.1}
-                    minZoom={0.5}
-                    maxZoom={2}
-                    userZoomingEnabled={true}
-                    cy={cy =>
-                    cy.layout(layout).run() // Apply the dagre layout
-                    } 
-                    elements = {Array.from(graph)} 
-                    style =  { {width: 1920, height: 1080} }
-                    stylesheet={style} // The different graph types.
-                    />;
+function App() {
+  return  (
+    <div>
+      <ThemeProvider>
+        <Router>
+          <Route path="/" exact component={Home} />
+        </Router>
+      </ThemeProvider>
+    </div>
+  );
 }
+
+
+export default App;
