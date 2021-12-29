e374089d02a95c85a1a3126ec0e587f66939e68b
Niki691 <nklaerum@t-online.de>
2021-12-29 12:05:50 +0100

    Show CSV Files in a Table

--- frontend/src/componentsCSV/Cards/Cards.js
+++ frontend/src/componentsCSV/Cards/Cards.js
@@ -3,8 +3,44 @@ import React from "react";
 import "./CardsCSV.css";
 import "@ui5/webcomponents-icons/dist/AllIcons.js";
 import "bootstrap/dist/css/bootstrap.min.css";
+import {
+  List,
+  CustomListItem,
+  Button,
+  Text,
+} from "@ui5/webcomponents-react";
+import { Icon } from '@ui5/webcomponents-react';
+import axios from 'axios';
+import { useState } from "react";
 
 export default function Cards() {
+
+  const [csvIds, setCSVIds] = useState([]);
+
+  async function getCSVIds() {
+    await axios
+      .get("/graph/csv/ids")
+      .then((response) => {
+        var csvMap = [];
+        response.data.forEach((element) => {
+          console.log(element._id);
+          csvMap.push({
+            id: element._id,
+            name: element.name
+          });
+        });
+        console.log(csvMap);
+        setCSVIds(csvMap);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }
+
+  // Use effect for the graph id fetching
+  React.useEffect(() => {
+    getCSVIds();
+  }, []);
   
   return (
     <>
@@ -18,12 +54,35 @@ export default function Cards() {
             <ui5-icon name="overview-chart" slot="avatar"></ui5-icon>
           </ui5-card-header>
           <div class="card-content">
-          <ui5-list id="myList" class="full-width">
-            <ui5-li icon="sap-icon://documents">Eventlog.csv</ui5-li>
-            <ui5-li icon="sap-icon://documents">Eventlog2.csv</ui5-li>
-            <ui5-li icon="sap-icon://documents">Eventlog3.csv</ui5-li>
-            <ui5-li icon="sap-icon://documents">Eventlog4.csv</ui5-li>
-          </ui5-list>
+          <List
+              separators="None"
+              class="card-content-child"
+              style={{ height: "320px" }}
+              //onItemClick={handleGraphItem}
+            >
+          {csvIds.map((listItem) => (
+                <CustomListItem key={listItem.id} data-id={listItem.id} >
+                  <div class="col1">
+                      <div>
+                       <Text>#{listItem.id}</Text>
+                      </div>
+                      <Icon name="documents"></Icon>
+                      <Text
+                        style={{ fontSize: "16px", fontWeight: "bold", marginLeft: '10px'}}
+                      >
+                        {listItem.name}
+                      </Text>
+                  </div>
+                  <div class="col2">
+                    <Button
+                      class="btn-del"
+                      icon="sap-icon://delete"
+                      //onClick={() => handleDelShow(listItem.id)}
+                    ></Button>
+                  </div>
+                </CustomListItem>
+              ))}
+            </List>
           </div>
         </ui5-card>
       </div>
--- frontend/src/componentsCSV/Subheader/Subheader.js
+++ frontend/src/componentsCSV/Subheader/Subheader.js
@@ -1,8 +1,10 @@
-import React, { useState } from "react";
+import React from "react";
 import "./SubheaderCSV.css";
 import ReactFileReader from 'react-file-reader';
+import axios from 'axios';
 
 export default function SubHeader({getCSV}) {
+  
 
   const handleFiles = files => {
     var reader = new FileReader();
@@ -10,6 +12,20 @@ export default function SubHeader({getCSV}) {
       getCSV(reader.result)
     }
     reader.readAsText(files[0]);
+    console.log(files[0])
+    const formData = new FormData();
+    formData.append('_id', 1)
+    formData.append('name', 'test1')
+    formData.append('file', files[0])
+    axios.post("http://192.168.178.27:8080/graph/csv/import", formData, {
+    })
+    .then(res => {
+      console.log(res.statusText)
+      console.log(res.headers)
+    })
+    .catch((err) => {
+      console.log(err);
+    });
   }
 
   return (
